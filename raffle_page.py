f8449e7084d7df4ce7442e0c4f0d045311458597
suhyuuk <joo0970@korea.ac.kr>
2021-07-22 13:02:37 +0900

    .

--- raffle_page.py
+++ raffle_page.py
@@ -2,20 +2,69 @@ import schedule
 import time
 import webbrowser
 
-def job():
+time_raffle = ["09:00", "19:00"]
+time_mail = ["09:00", "12:30", "15:00"]
+
+####################################################
+
+def job1():
     
-    url2= "https://store.musinsa.com/app/raffle/lists?raffle_state_gb=S"
+    # url2= "https://store.musinsa.com/app/raffle/lists?raffle_state_gb=S"
     url1= "https://www.shoeprize.com/"
-    url3= "https://www.luck-d.com/"
+    # url3= "https://www.luck-d.com/"
 
     webbrowser.open(url1) 
+    # webbrowser.open(url2)
+    # webbrowser.open(url3)
+
+def job2():
+    
+    url2= "https://store.musinsa.com/app/raffle/lists?raffle_state_gb=S"
+    # url1= "https://www.shoeprize.com/"
+    # url3= "https://www.luck-d.com/"
+
+    # webbrowser.open(url1) 
     webbrowser.open(url2)
+    # webbrowser.open(url3)
+
+
+def job3():
+    
+    # url2= "https://store.musinsa.com/app/raffle/lists?raffle_state_gb=S"
+    # url1= "https://www.shoeprize.com/"
+    url3= "https://www.luck-d.com/"
+
+    # webbrowser.open(url1) 
+    # webbrowser.open(url2)
     webbrowser.open(url3)
 
-schedule.every().day.at("12:30").do(job)
+
+def mail():
+
+    url_mail = "https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox"
+
+    webbrowser.open(url_mail)
+
+
+for times in time_mail:
+    schedule.every().day.at(times).do(mail)
+
+for times_2 in time_raffle:
+    schedule.every().day.at(times_2).do(job1)
+    schedule.every().day.at(times_2).do(job2)
+    schedule.every().day.at(times_2).do(job3)
+
 
 while True:
     schedule.run_pending()
     time.sleep(1)
 
-    #pyinstaller --noconsole --onefile raffle_page.py
+    
+
+
+
+#cd C:\Users\Stephen Kim\Documents\GitHub\raffle_scrapping
+#pyinstaller --noconsole --onefile raffle_page.py
+
+
+
